745f8b0f5c25801c72f013b6109039df6f1d6409
TatyanaStartseva <TatyanaStartseva2020@yandex.ru>
2026-04-11 18:56:43 +0300

    добавлен запрос на поиск номер, проверяется нет ли пересекающихся дат на это бронирование

--- src/repositories/base.py
+++ src/repositories/base.py
@@ -11,8 +11,8 @@ class BaseRepository:
         self.session = session
 
 
-    async def get_filtered(self, **filter_by):
-            query = select(self.model).filter_by(**filter_by)
+    async def get_filtered(self, *filter, **filter_by):
+            query = (select(self.model).filter(*filter).filter_by(**filter_by))
             result = await self.session.execute(query)
             return [self.schema.model_validate(model, from_attributes=True ) for model in result.scalars().all()] # model_validate переобразует orm объект в pydantic schemas, реализуем паттерн DataMapper.  from_attributes нужен чтобы из полей класса ORM привести к виду словаря pydantic
 
